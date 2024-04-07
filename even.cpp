13d3ee49d8264a06e9d35b79d0d67bdd3622bcb4
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-07 18:38:17 +0530

    Add files via upload

---
+++ even.cpp
@@ -0,0 +1,17 @@
+//To find the even number.
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num;
+	cout<<"enter a value : ";
+	cin>> num;
+	if(num % 2==0)
+	{
+		cout<<num<<"is an even no";
+		
+	}
+	return 0;
+}
+
+
