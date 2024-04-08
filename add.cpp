3d4c133600a3424b775a37052d41851a1fb15b8b
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-08 08:48:18 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,14 @@
+//All arithmatics using int
+#include<iostream>
+using namespace std;
+int main()
+    {
+        int x=5;
+        int y=4;
+        cout<<x+y<<endl;
+        cout<<x*y<<endl;
+        cout<<x-y<<endl;
+        cout<<x/y;
+
+    }
+   
