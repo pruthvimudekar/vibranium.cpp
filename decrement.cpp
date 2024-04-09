5618c58b39b4c0c78acf99a113eadedde7e227bc
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-09 22:19:09 +0530

    Add files via upload

---
+++ decrement.cpp
@@ -0,0 +1,11 @@
+//Decrementing the value with output
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x=4;
+	cout<<x<<endl; //4
+	cout<<x--<<endl;//4
+	cout<<x<<endl;//3
+	return 0;
+}
