96a5403d5a43387a0e1baa3392362df48a505a2b
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-11 00:11:35 +0530

    Create even odd.cpp

---
+++ even odd.cpp
@@ -0,0 +1,10 @@
+//even and odd number
+#include<iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<<"enter the number";
+cin>>n;
+if (n%2==0)<<cout<<"even number";
+if (n%2!=0)<<cout<<"odd number";
