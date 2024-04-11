fb1575f8e63c0792283c98055a2fb152ca8eae31
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-11 18:10:16 +0530

    Create absolute value.cpp

---
+++ absolute value.cpp
@@ -0,0 +1,12 @@
+//que. take integer input & print absolute value integer.
+//negative number hoga toh kaise change kre in positive numbers
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the n :";
+    cin>>n;
+if(n<0) n= -n;
+cout<<n;
+}
