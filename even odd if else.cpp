99d8b0645e763a4a551b5c2be30bfb60512f0020
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-11 17:49:27 +0530

    Create even odd if else.cpp

---
+++ even odd if else.cpp
@@ -0,0 +1,17 @@
+// To find even odd using if-else statment...
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the n :";
+    cin>>n;
+    if(n%2==0)
+    {
+        cout<<"even number";
+         }
+         else{
+             cout<<"odd number";
+         
+         }
+}
