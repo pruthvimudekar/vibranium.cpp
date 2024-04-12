57bd3dbae44a14b6d7fc49e79deb8099f570da7f
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-12 19:53:45 +0530

    Create three digit codes.cpp

---
+++ three digit codes.cpp
@@ -0,0 +1,18 @@
+// take a postive integer ip & and write code three digit code or not.
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter the n :";
+    cin>>n;
+    
+    if(n>99 && n<1000){// we can use and also..
+    
+    cout<<"Three digit number";
+
+    }
+    else{
+        cout<<"not a three digit number";
+    }
+}
