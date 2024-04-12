c01db40f563023fdcae23137afbf02a84646046e
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-12 21:26:08 +0530

    Create divisible code.cpp

---
+++ divisible code.cpp
@@ -0,0 +1,16 @@
+//divisible by 3 and 4 code..
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter the n";
+	cin>>n;
+	if(n%3==0 && n%5==0){
+		cout<<"it is an divisible by two no ";
+		
+	}
+	else{
+		cout<<"not divisible ";
+	}
+}
