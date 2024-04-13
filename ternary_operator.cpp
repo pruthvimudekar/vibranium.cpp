bada8d34a4017e320233372139336bf98e171171
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-04-13 22:44:15 +0530

    Create ternary_operator.cpp

---
+++ ternary_operator.cpp
@@ -0,0 +1,10 @@
+//ternary operator to save the lines....
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"enter n :";
+    cin>>n;
+    (n%2==0) ? cout<<"even" : cout<<"odd";
+}
