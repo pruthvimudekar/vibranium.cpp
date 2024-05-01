e4c26f40e69e77e0ef1b2734de67d3fc7eca7881
Pruthvi Mudekar <163886886+pruthvimudekar@users.noreply.github.com>
2024-05-01 22:40:18 +0530

    Add files via upload

---
+++ program.cpp
@@ -0,0 +1,23 @@
+//array 
+#include<iostream>
+using namespace std;
+int main()
+{
+ 
+    int a[10],i;
+    cout<<"enter the array element"<<endl;
+    for(i=0;i<=9;i++)
+    {
+        cin>>a[i];
+
+    }
+    cout<<"the entered array is ="<<endl;
+    for(i=0;i<=9;i++)
+{
+    cout<<a[i];
+}
+return 0;
+
+
+
+}
